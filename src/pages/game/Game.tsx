aed4175ff17dd340f7ede0392449eb467ed2251d
Austin <git@msg.austinh.io>
2024-06-21 07:26:59 -0700

    Fixed modal not being centered

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from 'react';
-import GameVisual from './GameVisual';
-import LetterTray from './LetterTray';
-import { motion } from 'framer-motion';
-import Button from '../../components/Button';
-import WordDisplay from './WordDisplay';
-import Modal from '../../components/Modal';
-import GameWindow from './GameWindow';
+import { useEffect, useState } from "react";
+import GameVisual from "./GameVisual";
+import LetterTray from "./LetterTray";
+import { motion } from "framer-motion";
+import Button from "../../components/Button";
+import WordDisplay from "./WordDisplay";
+import Modal from "../../components/Modal";
+import GameWindow from "./GameWindow";
 
 const pageTransition = {
   initial: {
-    x: '100vw',
+    x: "100vw",
   },
   animate: {
     x: 0,
     transition: {
       duration: 0.5,
-      ease: 'easeOut',
+      ease: "easeOut",
     },
   },
   exit: {
-    x: '-100vw',
+    x: "-100vw",
     transition: {
       duration: 0.5,
     },
@@ -28,13 +28,13 @@ const pageTransition = {
 
 const initGuessCount = 3;
 
-const wordApiSource = 'https://random-word-api.herokuapp.com/word?number=1';
+const wordApiSource = "https://random-word-api.herokuapp.com/word?number=1";
 
 const Game: React.FC = () => {
-  const [word, setWord] = useState<string>('');
+  const [word, setWord] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [fetchNewWord, setFetchNewWord] = useState<boolean>(true);
-  const [error, setError] = useState<string>('');
+  const [error, setError] = useState<string>("");
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   const [isNewGame, setIsNewGame] = useState<boolean>(true);
   const [guessCount, setGuessCount] = useState<number>(initGuessCount);
@@ -64,9 +64,9 @@ const Game: React.FC = () => {
 
   function handleEndGame() {
     setIsNewGame(true);
-    setWord('');
+    setWord("");
     resetGuessCount();
-    setEnableModal(() => true)
+    setEnableModal(() => true);
   }
 
   function handleSetLastLetter(letter: string) {
@@ -74,7 +74,7 @@ const Game: React.FC = () => {
   }
 
   useEffect(() => {
-    document.title = 'Game | WordGuess';
+    document.title = "Game | WordGuess";
   });
 
   useEffect(() => {
@@ -92,12 +92,12 @@ const Game: React.FC = () => {
         const response = await fetch(wordApiSource);
 
         if (!response.ok) {
-          throw new Error('Something went wrong');
+          throw new Error("Something went wrong");
         }
 
         const data = await response.json();
 
-        if (!data) throw new Error('No word recieved');
+        if (!data) throw new Error("No word recieved");
 
         setWord(data[0]);
       } catch (error: any) {
@@ -111,85 +111,84 @@ const Game: React.FC = () => {
   }, [fetchNewWord]);
 
   return (
-    <motion.div
-      initial='initial'
-      animate='animate'
-      exit='exit'
-      variants={pageTransition}
-      className='flex flex-col w-full max-w-7xl px-4'>
-      <Modal
-        enabled={enableModal}>
+    <>
+      <Modal enabled={enableModal}>
         <h4>Modal</h4>
         <p>Lorem Ipsum</p>
         <Button onClick={() => setEnableModal(false)}>Close Me</Button>
       </Modal>
-      <div className='flex flex-col mt-8'>
-        <main className='flex flex-col lg:flex-row items-center gap-8'>
-          <GameVisual>
-            {isNewGame && !isLoading && !error && (
-              <p className='text-2xl'>
-                Press <b>New Game</b> to start
-              </p>
-            )}
-            {!isNewGame && isLoading && !error && (
-              <p className='text-2xl'>
-                <i>Getting new word...</i>
-              </p>
-            )}
-            {!isNewGame && !isLoading && !error && word && (
-              <>
-                <GameWindow />
-                <WordDisplay
-                  word={word}
-                  guessedLetters={guessedLetters}
-                />
-              </>
-            )}
-            {error && <p className='text-2xl'>{`${error}`}</p>}
-          </GameVisual>
-          <div className='flex flex-col w-full flex-grow'>
-            <div className='flex justify-between items-center'>
-              <p className='sm:text-2xl text-lg'>
-                Guesses left: <b>{guessCount}</b>
-              </p>
-              <Button
-                onClick={isNewGame ? handleStartNewGame : handleEndGame}
-                type={isNewGame ? 'primary' : 'danger'}>
-                {isNewGame ? 'New Game' : 'End Game'}
-              </Button>
-            </div>
-            <div className='mt-8'>
-              <LetterTray
-                onHandleClick={handleSetLastLetter}
-                onHandleGuessWrong={handleDecrementGuessCount}
-                isNewGame={isNewGame}
-                isLoading={isLoading}
-                word={word}
-              />
-            </div>
-
-            <div className='flex gap-8 items-center border-solid border-2 rounded-md p-4 mt-24 mr-auto sm:mb-auto mb-8'>
-              <Button
-                onClick={handleSetDebugMode}
-                type='danger'>
-                Cheat Mode
-              </Button>
-              {
-                <p className={` text-red-200 font-bold`}>
-                  The word is{' '}
-                  <i
-                    className={` font-normal transition-all ${
-                      !debugMode ? 'blur' : 'blur-none'
-                    }`}>
-                    {!!word ? word : '(n/a)'}
-                  </i>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={pageTransition}
+        className="flex flex-col w-full max-w-7xl px-4"
+      >
+        <div className="flex flex-col mt-8">
+          <main className="flex flex-col lg:flex-row items-center gap-8">
+            <GameVisual>
+              {isNewGame && !isLoading && !error && (
+                <p className="text-2xl">
+                  Press <b>New Game</b> to start
                 </p>
-              }
+              )}
+              {!isNewGame && isLoading && !error && (
+                <p className="text-2xl">
+                  <i>Getting new word...</i>
+                </p>
+              )}
+              {!isNewGame && !isLoading && !error && word && (
+                <>
+                  <GameWindow />
+                  <WordDisplay word={word} guessedLetters={guessedLetters} />
+                </>
+              )}
+              {error && <p className="text-2xl">{`${error}`}</p>}
+            </GameVisual>
+            <div className="flex flex-col w-full flex-grow">
+              <div className="flex justify-between items-center">
+                <p className="sm:text-2xl text-lg">
+                  Guesses left: <b>{guessCount}</b>
+                </p>
+                <Button
+                  onClick={isNewGame ? handleStartNewGame : handleEndGame}
+                  type={isNewGame ? "primary" : "danger"}
+                >
+                  {isNewGame ? "New Game" : "End Game"}
+                </Button>
+              </div>
+              <div className="mt-8">
+                <LetterTray
+                  onHandleClick={handleSetLastLetter}
+                  onHandleGuessWrong={handleDecrementGuessCount}
+                  isNewGame={isNewGame}
+                  isLoading={isLoading}
+                  word={word}
+                />
+              </div>
+
+              <div className="flex gap-8 items-center border-solid border-2 rounded-md p-4 mt-24 mr-auto sm:mb-auto mb-8">
+                <Button onClick={handleSetDebugMode} type="danger">
+                  Cheat Mode
+                </Button>
+                {
+                  <p className={` text-red-200 font-bold`}>
+                    The word is{" "}
+                    <i
+                      className={` font-normal transition-all ${
+                        !debugMode ? "blur" : "blur-none"
+                      }`}
+                    >
+                      {!!word ? word : "(n/a)"}
+                    </i>
+                  </p>
+                }
+              </div>
             </div>
-          </div>
-        </main>
-      </div>
-    </motion.div>
+          </main>
+        </div>
+      </motion.div>
+    </>
   );
 };
 
